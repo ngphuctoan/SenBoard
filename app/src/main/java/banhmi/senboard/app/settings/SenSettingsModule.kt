d771aa3cc3a8d4bd5a4becee5137b26ab6a6072f
Im-Karl <phananhkiet2k5@gmail.com>
2026-08-13 20:47:56 +0700

    add: help screen

--- app/src/main/java/banhmi/senboard/app/settings/SenSettingsModule.kt
+++ app/src/main/java/banhmi/senboard/app/settings/SenSettingsModule.kt
@@ -3,8 +3,9 @@ package banhmi.senboard.app.settings
 import banhmi.senboard.app.navigation.SenEntryProviderInstaller
 import banhmi.senboard.app.navigation.SenNavigator
 import banhmi.senboard.app.navigation.SenSettingsAbout
-import banhmi.senboard.app.navigation.SenSettingsHome
 import banhmi.senboard.app.navigation.SenSettingsHaptics
+import banhmi.senboard.app.navigation.SenSettingsHelp
+import banhmi.senboard.app.navigation.SenSettingsHome
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,6 +20,9 @@ object SenSettingsModule {
     fun provideEntryProviderInstaller(navigator: SenNavigator): SenEntryProviderInstaller = {
         entry<SenSettingsHome> {
             SenSettingsHomeScreen(onNavigate = { route -> navigator.goTo(route) })
+        }
+        entry<SenSettingsHelp> {
+            SenSettingsHelpScreen(onNavigateBack = { navigator.goBack() })
         }
         entry<SenSettingsAbout> {
             SenSettingsAboutScreen(onNavigateBack = { navigator.goBack() })
